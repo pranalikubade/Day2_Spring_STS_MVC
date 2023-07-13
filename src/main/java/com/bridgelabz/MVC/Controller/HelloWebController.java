49da2f553ccb83acb6410fbd8ea6c2bbc972a6dd
pranalikubade <118585763+pranalikubade@users.noreply.github.com>
2023-07-13 16:55:45 +0530

    Update HelloWebController.java

--- src/main/java/com/bridgelabz/MVC/Controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/MVC/Controller/HelloWebController.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.MVC.Controller;
 
-import org.apache.catalina.User;
+import com.bridgelabz.MVC.Model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
